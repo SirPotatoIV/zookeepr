95589878a006d8b2ba1930bd94ed68789ddfc704
Jake O'Toole <otoolej33@gmail.com>
2020-06-23 15:41:00 -0500

    query filtering added implemented

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
 const express = require('express');
-const animals = require('./data/animals.json');
+const { animals } = require('./data/animals.json');
 
 const app = express();
 
+function filterByQuery(query, animalsArray){
+    let personalityTraitsArray = [];
+    // setting filteredResults equal to animalsArray
+    let filteredResults = animalsArray;
+
+    // handle queries for personality traits
+    if(query.personalityTraits) {
+        if(typeof query.personalityTraits === 'string'){
+            personalityTraitsArray = [query.personalityTraits];
+        } else{
+            personalityTraitsArray = query.personalityTraits;
+        }
+    }
+    // Loop through each trait in the personalityTraits array:
+    personalityTraitsArray.forEach(trait => {
+        filteredResults = filteredResults.filter(
+            animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+    });
+    if(query.diet){
+        fitleredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if(query.species){
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if(query.name){
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
 app.get('/api/animals', (req, res) => {
-    res.json(animals);
+    let results = animals;
+    if(req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 })
 
 app.listen(3001, ()=>{
